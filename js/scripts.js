c40896ae77be6f293afde3c85ad41f27f5b9818a
Carlos Vargas <csv17@live.unc.edu>
2018-09-24 14:41:36 -0400

    trying to fix the hamburger menu, but we couldnt

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,35 @@
 console.log('Javascript Loaded');
 
 var burger = document.getElementById('burger');
-var mobilenav = document.getElementById('offscreen-nav');
+var mobileNav = document.getElementById('offscreen-nav');
 var closebtn = document.getElementById('close-btn');
 var content = document.getElementById('content');
 var moreInfoText = document.getElementById('more-info');
 var tooltip = document.getElementById('tool-tip');
 
-
 burger.addEventListener('click', function(){
-    console.log('button clicked');
-    mobilenav.style.width= '50%';
-    mobilenav.style.transition = '0.5s ease';
-} );
+    console.log("buton clicked");
+    mobileNav.style.display = "block";
+
+    mobileNav.style.background = "red";
+
+    mobileNav.style.transition = '0.5s ease';
+    //content.style.opacity='0.1';
+
+});
 
 closebtn = document.addEventListener('click', function(){
-mobilenav.style.width= '0';
-mobilenav.style.transition= '0.5s ease';
+mobileNav.style.width= '0';
+mobileNav.style.transition= '0.5s ease';
 content.style.opacity= '1';
-
-}
-
-);
+});
 
 moreInfoText.addEventListener("mouseenter", function(event){
     tooltip.style.opacity='1';
-    var x = event.pageX;
-    var y = event.pageY;
+    var x = event.screenX;
+    var y = event.screenY;
     console.log(y);
-    
+    tooltip.style.top = y +'px';
+    tooltip.style.left = x + 'px';
 }
 );
