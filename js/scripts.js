c8949d2d764b854b82778b830f056ff9552c9d8c
CarlosVargasUS <csv17@live.unc.edu>
2018-09-25 19:34:47 -0400

    side bar working

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,33 @@
 console.log('Javascript Loaded');
 
 var burger = document.getElementById('burger');
-var mobileNav = document.getElementById('offscreen-nav');
+var mobileNav = document.getElementsByClassName('no-show-bar');
+var lists = document.getElementsByClassName('list');
 var closebtn = document.getElementById('close-btn');
 var content = document.getElementById('content');
 var moreInfoText = document.getElementById('more-info');
 var tooltip = document.getElementById('tool-tip');
 
 burger.addEventListener('click', function(){
-    console.log("buton clicked");
-    mobileNav.style.display = "block";
 
-    mobileNav.style.background = "red";
-
-    mobileNav.style.transition = '0.5s ease';
-    //content.style.opacity='0.1';
+    console.log("button clicked");
+    mobileNav[0].style.width = '50%';
+    mobileNav[0].style.transition = '0.5s ease';
+    content.style.opacity = '1'
 
 });
 
-closebtn = document.addEventListener('click', function(){
-mobileNav.style.width= '0';
-mobileNav.style.transition= '0.5s ease';
-content.style.opacity= '1';
+closebtn.addEventListener('click', function(){
+mobileNav[0].style.width = '0%';
+mobileNav[0].style.transition= '0.5s ease';
 });
 
 moreInfoText.addEventListener("mouseenter", function(event){
-    tooltip.style.opacity='1';
-    var x = event.screenX;
-    var y = event.screenY;
-    console.log(y);
-    tooltip.style.top = y +'px';
-    tooltip.style.left = x + 'px';
+    //tooltip.style.opacity='1';
+    //var x = event.screenX;
+    //var y = event.screenY;
+    //console.log(y);
+    //tooltip.style.top = y +'px';
+    //tooltip.style.left = x + 'px';
 }
 );
